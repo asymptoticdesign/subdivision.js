9bbb099d76b1c7441709d7c03a293192c31c0689
nathan lachenmyer <asymptoticdesign@mit.edu>
2013-03-12 17:31:33 -0400

    First attempt at a half-edge data structure: failure.  Unknown reason.

--- subdivision.js
+++ subdivision.js
@@ -7,20 +7,21 @@
  * A javascript library for computational geometry.
  */
 
-function Vertex(x, y) {
-    this.x = x;
-    this.y = y;
+function Vertex(_x, _y) {
+    this.x = _x;
+    this.y = _y;
+    this.edge;
 }
 
 function Face() {
     this.vertices = [];
     this.edges = [];
+    this.edge;
     this.centroid;
 }
 
-Face.prototype.addVertex = function(vertex) {
-    this.vertices.push(vertex);
-    return vertex;
+Face.prototype.addVertex = function(_vertex) {
+    this.vertices.push(_vertex);
 }
 
 Face.prototype.computeCentroid = function() {
@@ -59,34 +60,87 @@ Face.prototype.sortVertices = function() {
     }
 }
 
-Face.prototype.makeEdges = function() {
-    /*
-     * Sorts the vertices and then creates the edge map from the vertex data.
-     */
-    this.sortVertices();
-    for(var i = 0; i < this.vertices.length - 1; i++) {
-	var edge = new HalfEdge(this.vertices[i], this.vertices[i+1]);
-	edge.face = this;
-	this.edges.push(edge)
-    }
-    //manually add the last edge that goes from the last vertex to the first one
-    var edge = new HalfEdge(this.vertices[this.vertices.length - 1], this.vertices[0]);
-    edge.face = this;
-    this.edges.push(edge);
+Face.prototype.subdivide = function() {
 }
 
-function HalfEdge(vertex_a, vertex_b) {
-    this.vertices = [vertex_a, vertex_b];
+function HalfEdge(_vertex) {
+    this.origin = _vertex;
     this.face;
+    this.twin;
+    this.next;
+    this.prev;
 }
 
+function GeometryManager(_faces) {
+    this.faces = _faces;
+    this.vertices = _faces.vertices;
+    this.halfedges = [];
+    this.twinList = [];
+}
+
+GeometryManager.prototype.makeEdges = function() {
+    /*
+     * Creates the half-edge data structure from a list of faces (and their associated vertices).  This function takes each face, sorts the vertices, and creates half-edges in a loop around that face.  The half-edges are temporarily stored in the variable faceEdges until the ring is complete, and then the .next and .prev pointers are added before the faceEdges are added to the global list of edges (this.halfedges).  The half-edge pairs are created simultaneously, with the twin being placed in a list of spare twins.  Before creating a new edge, we always search the twinList for any edges that already represent the edge we're looking for.
+     */
+    for(var i = 0; i < this.faces.length; i++) {
+	var faceEdges = [];
+	face = this.faces[i];
+	face.sortVertices();
+	//for all vertices in the face
+	for(var j = 0; j < face.vertices.length; j++) {
+	    if(this.twinList) {
+		for(var k = 0; k < this.twinList.length; k++) {
+		    //loop over all existing edges created as twins
+		    if (face.vertices[j] == twinList[k].origin) {
+			//if this vertex is the origin of a twin, use that twin
+			twinList[k].face = face;
+			face.edge = twinList[k];
+			faceEdges.push(twinList[k]);
+		    }
+		    else {
+			//if we can't find a twin that belong to this vertex, then create both the twin and new halfedge from scratch.
+			var halfedge = new HalfEdge(face.vertices[j]);
+			halfedge.face = face;
+			face.edge = halfedge;
+			faceEdges.push(halfedge);
+			//create the twin
+			var twin = new HalfEdge(this.vertices[(j+1) % this.vertices.length]);
+			twin.face = undefined;
+			twinList.push(twin);
+		    }
+		}
+	    }
+	    else {
+		var halfedge = new HalfEdge(face.vertices[j]);
+		halfedge.face = face;
+		face.edge = halfedge;
+		faceEdges.push(halfedge);
+		//create the twin
+		var twin = new HalfEdge(this.vertices[(j+1) % this.vertices.length]);
+		twin.face = undefined;
+		twinList.push(twin);
+	    }
+	}
+	//and then add the next and previous pointers
+	var edgeListLength = this.halfedges.length;
+	for(var j = 0; j < edgeListLength; j++) {
+	    console.log((j-1) % edgeListLength);
+	    faceEdges[j].next = faceEdges[(((j+1) % edgeListLength) + edgeListLength) % edgeListLength];
+	    faceEdges[j].prev = faceEdges[(((j-1) % edgeListLength) + edgeListLength) % edgeListLength];
+	}
+	this.halfedges.push(faceEdges);
+    }
+}
+    
 /* Example! */
 
 //create vertices in a random order
-a = new Vertex(0,0);
-b = new Vertex(1,5);
-c = new Vertex(9,8);
-d = new Vertex(4,-1);
+a = new Vertex(0,-5);
+b = new Vertex(0,5);
+c = new Vertex(5,10);
+d = new Vertex(5,-10);
+e = new Vertex(-5,10);
+f = new Vertex(-5,-10);
 
 //create a face out of vertices
 f = new Face();
@@ -94,5 +148,10 @@ f.addVertex(a);
 f.addVertex(c);
 f.addVertex(b);
 f.addVertex(d);
-f.makeEdges();
-console.log(f.edges)
+g = new Face();
+g.addVertex(a);
+g.addVertex(b);
+g.addVertex(e);
+g.addVertex(f);
+geo = new GeometryManager([f,g]);
+geo.makeEdges();
